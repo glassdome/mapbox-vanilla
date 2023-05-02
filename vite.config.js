4e79fa9f40f7c057a5332e3ecfe4372e6c253383
sy <sy.smythe@gmail.com>
2023-05-02 10:17:22 -0400

    moved files to accomodate production build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mapbox-vanilla/'
-})
+  base: '/mapbox-vanilla/',
+  assetsInclude: ['**/*.geojson'],
+  build: {
+    emptyOutDir: true,
+    // assetsInclude: ['**/*.geojson'],
+  //   rollupOptions: {
+  //     output: {
+        
+  //       assetFileNames: ({ name }) => {
+  //         // if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
+  //         //     return 'assets/images/[name]-[hash][extname]';
+  //         // }
+
+  //         // if (/\.css$/.test(name ?? '')) {
+  //         //     return 'assets/css/[name]-[hash][extname]';
+  //         // }
+  //         if (/\.geojson$/.test(name ?? '')) {
+  //           return 'assets/data/[name]-[hash][extname]';
+  //         }
+  //         // default value
+  //         // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+  //         return 'assets/[name]-[hash][extname]';
+  //       },
+  //     },
+  //   },
+  },
+});
