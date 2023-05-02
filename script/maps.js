e964b0b68b27b26c53e85dad66ab4a00c8875ef3
sy <sy.smythe@gmail.com>
2023-05-02 17:54:27 -0400

    Add hover effect to Australian states

--- script/maps.js
+++ script/maps.js
@@ -32,13 +32,12 @@ const marker1 = new mapboxgl.Marker()
   .addTo(map);
 
 map.on('load', () => {
-  
   // console.log(map.getStyle());
 
   // Add Australian outline
   map.addSource('australia-outline', {
     type: 'geojson',
-    data: './data/australia/australia-outline.geojson'
+    data: './data/australia/australia-outline.geojson',
   });
 
   // map.addLayer({
@@ -57,15 +56,15 @@ map.on('load', () => {
     source: 'australia-outline',
     paint: {
       'line-color': 'white',
-      'line-width': 3
-    }
+      'line-width': 3,
+    },
   });
 
   // Add Australian State Boundaries
   map.addSource('australia-states-outline', {
     type: 'geojson',
-    data: './data/australia/australia-states-outline.geojson'
-  })
+    data: './data/australia/australia-states-outline.geojson',
+  });
 
   map.addLayer({
     id: 'australia-states-outline-line',
@@ -73,59 +72,115 @@ map.on('load', () => {
     source: 'australia-states-outline',
     paint: {
       'line-color': 'white',
-      'line-width': 3
+      'line-width': 3,
+    },
+  });
+
+  map.addLayer(
+    {
+      id: 'australia-states-outline-fill',
+      type: 'fill',
+      source: 'australia-states-outline',
+      paint: {
+        'fill-color': [
+          'match',
+          ['get', 'STATE_CODE'],
+          '1',
+          '#ff6961',
+          '2',
+          '#ffb480',
+          '3',
+          '#f8f38d',
+          '4',
+          '#42d6a4',
+          '5',
+          '#08cad1',
+          '6',
+          '#f8f38d',
+          '7',
+          '#ff6961',
+          '8',
+          '#42d6a4',
+          'red',
+        ],
+        'fill-opacity': [
+          'case',
+          ['boolean', ['feature-state', 'hover'], false],
+          0.5,
+          1,
+        ],
+      },
+
+      // Note: in the above 'match' block, the last color 'red' is the default if there are
+      // any items that were not explicitly matched. It seems to be MANDATORY. None of the
+      // other colors show up without it.
+    },
+    'road-rail'
+  );
+
+  let hoveredStateId = null;
+  map.on('mousemove', 'australia-states-outline-fill', (e) => {
+    if (e.features.length > 0) {
+      if (hoveredStateId !== null) {
+        map.setFeatureState(
+          { source: 'australia-states-outline', id: hoveredStateId },
+          { hover: false }
+        );
+      }
+      hoveredStateId = e.features[0].id;
+      map.setFeatureState(
+        { source: 'australia-states-outline', id: hoveredStateId },
+        { hover: true }
+      );
     }
   });
 
-  map.addLayer({
-    id: 'australia-states-outline-fill',
-    type: 'fill',
-    source: 'australia-states-outline',
-    paint: {
-      'fill-color': [
-        'match', ['get', 'STATE_CODE'],
-        '1', '#ff6961', 
-        '2', '#ffb480', 
-        '3', '#f8f38d', 
-        '4', '#42d6a4', 
-        '5', '#08cad1',
-        '6', '#f8f38d',
-        '7', '#ff6961', 
-        '8', '#42d6a4',
-        'red'
-      ]
+  // When the mouse leaves the state-fill layer, update the feature state of the
+  // previously hovered feature.
+  map.on('mouseleave', 'australia-states-outline-fill', () => {
+    if (hoveredStateId !== null) {
+      map.setFeatureState(
+        { source: 'australia-states-outline', id: hoveredStateId },
+        { hover: false }
+      );
     }
-
-    // Note: in the above 'match' block, the last color 'red' is the default if there are
-    // any items that were not explicitly matched. It seems to be MANDATORY. None of the
-    // other colors show up without it.
-  }, 'road-rail');
+    hoveredStateId = null;
+  });
 
   // Add NYC Borough Boundaries data source.
   map.addSource('nyc-borough-boundaries', {
     type: 'geojson',
-    data: './data/nyc-borough-boundaries.geojson'
+    data: './data/nyc-borough-boundaries.geojson',
   });
 
-  map.addLayer({
-    id: 'nyc-bourough-boundaries-fill',
-    type: 'fill',
-    source: 'nyc-borough-boundaries',
-    paint: {
-      'fill-color': [
-        'match', ['get', 'boro_code'],
-        1, 'green',
-        2, 'purple',
-        3, 'steelblue',
-        4, 'orange',
-        5, 'pink',
-        'red'
-      ]
-    }
-    // Note: in the above 'match' block, the last color 'red' is the default if there are
-    // any items that were not explicitly matched. It seems to be MANDATORY. None of the
-    // other colors show up without it.
-  }, 'road-rail');
+  map.addLayer(
+    {
+      id: 'nyc-bourough-boundaries-fill',
+      type: 'fill',
+      source: 'nyc-borough-boundaries',
+      paint: {
+        'fill-color': [
+          'match',
+          ['get', 'boro_code'],
+          1,
+          'green',
+          2,
+          'purple',
+          3,
+          'steelblue',
+          4,
+          'orange',
+          5,
+          'pink',
+          'red',
+        ],
+      },
+      // Note: in the above 'match' block, the last color 'red' is the default if there are
+      // any items that were not explicitly matched. It seems to be MANDATORY. None of the
+      // other colors show up without it.
+    },
+    'road-rail'
+  );
 
   map.addLayer({
     id: 'nyc-bourough-boundaries-line',
@@ -133,36 +188,34 @@ map.on('load', () => {
     source: 'nyc-borough-boundaries',
     paint: {
       'line-color': 'white',
-      'line-width': 2
-    }
+      'line-width': 2,
+    },
   });
 
   map.on('click', (e) => {
-    
-    const [ selected ] = map.queryRenderedFeatures(e.point, {
-      layers: ['nyc-bourough-boundaries-fill']
-    })
+    const [selected] = map.queryRenderedFeatures(e.point, {
+      layers: ['nyc-bourough-boundaries-fill'],
+    });
 
     if (selected) {
-      const name = selected.properties.boro_name
-      const population = selected.properties.pop_2020
-      alert(`The population of ${name} is ${population}`)
+      const name = selected.properties.boro_name;
+      const population = selected.properties.pop_2020;
+      alert(`The population of ${name} is ${population}`);
     }
-
   });
 
   const flyTo = (location) => {
     map.flyTo({
       center: location,
-      essential: true
-    })
-  }
-  
-  const usaSouthWest = [-121.170444,27.721980]
-  const usaNorthWest = [-67.338467,47.079475]
+      essential: true,
+    });
+  };
 
-  const ausSouthWest = [-246.261292,-42.630675]
-  const ausNorthWest = [-208.117962,-11.138993]
+  const usaSouthWest = [-121.170444, 27.72198];
+  const usaNorthWest = [-67.338467, 47.079475];
+
+  const ausSouthWest = [-246.261292, -42.630675];
+  const ausNorthWest = [-208.117962, -11.138993];
 
   document.getElementById('fly-usa').addEventListener('click', () => {
     map.fitBounds([usaSouthWest, usaNorthWest]);
@@ -176,6 +229,5 @@ map.on('load', () => {
     // });
     map.fitBounds([ausSouthWest, ausNorthWest]);
   });
-
 })
 
